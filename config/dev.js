324ede8954e49adc511119225050238b63dd6ee6
Jeromeski <jeromegee@gmail.com>
2020-03-16 17:48:34 +0800

    Passport initial setup, GoogleStrategy initial config, created keys folder to house dev and prod keys

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '537413721141-a2riq57a30i2g7g3g015pp72j054vdbq.apps.googleusercontent.com',
+  googleClientSecret: '1ZF5q_9uBWG6gTxOY5ZFrheH',
+  mongoURI: 'mongodb+srv://jerome:password123!@coco-v6fob.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'BlIsoECYe0'
+};
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const express = require('express');
+const express = require("express");
+const passport = passport();
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  send.res({ hi: 'there '})
-})
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ _root: "I am root" });
+});
 
 const PORT = 5000;
 
-app.listen(PORT)
+app.listen(PORT);
 
 console.log(
-`==============================
+  `==============================
   Server is running at ${PORT}
 ==============================`
 );
-  
