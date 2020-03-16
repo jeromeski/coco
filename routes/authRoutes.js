b047cce16e120df95dbcb09c52de816b605fe5fe
Jeromeski <jeromegee@gmail.com>
2020-03-16 19:40:40 +0800

    Refactored server code structure.

---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+
+  app.get("/", (req, res) => {
+    res.send({ _root: "I am root" });
+  });
+  
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+  
+  app.get('/auth/google/callback', passport.authenticate('google'));
+}
