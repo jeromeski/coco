4df155445e77548a154ee8572db9dec1bf5b00c5
Jeromeski <jeromegee@gmail.com>
2020-03-16 19:57:54 +0800

    Installed mongoose, applied init config and connected it to mongoDB.

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
-require('./services/passport');
+require("./services/passport");
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(err => console.log(err))
+  .then(res => console.log("#### mongoDB is connected ####"));
 
 const app = express();
 
 // const authRoutes = require('./routes/authRoutes');
 // authRoutes(app);
-
-require('./routes/authRoutes')(app)
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
 
 console.log(
   `==============================
