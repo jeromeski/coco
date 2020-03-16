e2bc394389140ab36c047455f48a5206852955a0
Jeromeski <jeromegee@gmail.com>
2020-03-16 18:08:21 +0800

    Installed passport, utilized and init config of GoogleStrategy, created auth/google handler

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require("express");
-const passport = passport();
+const passport = require('passport');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys");
 
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
-  }, (accessToken) => {
-    console.log(accessToken);
-  })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
 );
 
 app.get("/", (req, res) => {
   res.send({ _root: "I am root" });
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = 5000;
 
