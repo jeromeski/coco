31e3e341d996f20919703b670662a3fd54a30ee3
Jeromeski <jeromegee@gmail.com>
2020-03-17 05:25:26 +0800

    fixed new MissingSchemaError,  saving mongoose User model in passport

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,11 @@
-const passport = require('passport');
+const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+// 1 arg means you want to fetch somethin in mongoose
+// 2 args means you wanna load
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +15,8 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('## accessToken ## :', accessToken);
-      console.log('## refreshToken ## :', refreshToken);
-      console.log('## profile ## :', profile);
+      // Get access to the mongo model here
+      new User({ googleId: profile.id }).save();
     }
   )
 );
