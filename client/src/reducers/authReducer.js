4f9f9f8d01c888b734f7294400a86c77c08bb121
Jeromeski <jeromegee@gmail.com>
2020-03-18 05:09:33 +0800

    Refactored fetchUser action creator, removed console.log

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,12 @@
 import { FETCH_USER } from "../actions/types";
 
 export default function(state = {}, action) {
-  console.log(action)
   switch (action.type) {
     case FETCH_USER: {
       return {
         ...state,
         auth: action.payload
-      }
+      };
     }
     default:
       return state;
