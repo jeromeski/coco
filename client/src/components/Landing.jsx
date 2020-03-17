28d7a39bf9ea82f2e7dda273a24325b040890f20
Jeromeski <jeromegee@gmail.com>
2020-03-18 07:21:29 +0800

    Redirect logout to Landing, Landing page basic styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col className='text-center'>
+          <h1 className='mt-5 display-1 font-weight-bold'>Coco</h1>
+          <h3>Collect surveys from your clients</h3>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
