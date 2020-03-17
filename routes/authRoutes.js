9ef3409c67531c641624fdd0139d205b59f138af
Jeromeski <jeromegee@gmail.com>
2020-03-18 07:02:35 +0800

    Redirecting user upon Auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,14 +12,20 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
-  
-  app.get('/api/logout', (req, res) => {
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
+
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send({im: 'logged out!'});
+    res.send({ im: "logged out!" });
   });
 
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 };
