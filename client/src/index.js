dfbc92d1e3cde98b3f008b1128cfdadd9cc5e8df
Jeromeski <jeromegee@gmail.com>
2020-03-17 15:19:03 +0800

    created actions folder, actions creator (index) and actions type

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import App from "./components/App";
 import reducers from "./reducers";
+import reduxThunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(() => reducers, {}, (applyMiddleware()));
+const store = createStore(() => reducers, {}, (applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
