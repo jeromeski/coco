b5cf74f5811945153220e9550ed0bd59f012706f
Jeromeski <jeromegee@gmail.com>
2020-03-17 10:29:43 +0800

    Installed CRA and fixed and configured proxy problem

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware }  = require('http-proxy-middleware')
+
+const proxy = createProxyMiddleware;
+
+module.exports = function(app) {
+   app.use(proxy(['/api', '/auth/google'], { target: 'http://localhost:5000' }));
+}
