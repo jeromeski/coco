df874f31ba99c67ec386996535d5b7d731123a99
Jeromeski <jeromegee@gmail.com>
2020-03-18 14:09:46 +0800

    Payment component [Updates] name, desc and button.

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import {Button} from 'react-bootstrap';
 
 
 class Payments extends Component {
@@ -7,10 +8,14 @@ class Payments extends Component {
     return (
       <div>
         <StripeCheckout
+          name='coco'
+          description='$5 for 5 email credits' 
           amount={500}
           token={token => console.log(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        />
+        >
+          <Button size='sm'>Add Credits</Button>
+        </StripeCheckout>
       </div>
     );
   }
