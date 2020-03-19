e709818f48426ac24f82c3b812cba7d17d92ee48
Jeromeski <jeromegee@gmail.com>
2020-03-19 17:30:39 +0800

    Setting up Sub Docs and Relationship fields

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
 const {Schema}  = mongoose;
+const RecipientSchema = require('./Recipient');
+
 
 const surveySchema = new Schema ({
   title: String,
   body: String,
   subject: String,
-  recepients: [String]
+  recepients: [RecipientSchema],
+  yes: {type: Number, default: 0},
+  no: {type: Number, default: 0},
+  _user: { type: Schema.Types.ObjectId, ref: 'User'},
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
