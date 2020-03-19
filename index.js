00959a17821789f724e6448bbd7a32b517618c6d
Jeromeski <jeromegee@gmail.com>
2020-03-19 12:22:58 +0800

    updated static path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cookieSession = require('cookie-session');
-const passport = require('passport')
-const bodyParser = require('body-parser');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 const mongoose = require("mongoose");
 require("./models/User");
@@ -17,14 +17,14 @@ mongoose
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -32,15 +32,14 @@ app.use(passport.session());
 // const authRoutes = require('./routes/authRoutes');
 // authRoutes(app);
 require("./routes/authRoutes")(app);
-require('./routes/billingRoutes')(app);
+require("./routes/billingRoutes")(app);
 
-if(process.env.NODE_ENV === 'production' ) {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-
-  const path = require('path')
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
